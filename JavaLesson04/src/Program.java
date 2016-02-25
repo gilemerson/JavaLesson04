ea78e0599ca34896e2aac570e94c394ca40d95b4
Gil Emerson <emerson-gil@outlook.com>
2016-02-24 21:11:01 -0500

    While & Try Catch Block Examples

--- JavaLesson04/src/Program.java
+++ JavaLesson04/src/Program.java
@@ -1,9 +1,31 @@
+import java.util.Scanner;
 
 public class Program {
 
+	//Try Catch Blocks 
+	
 	public static void main(String[] args) {
+			
+		int myAge;
+		boolean isValid = false;
 		
+		while (!isValid) {
+			
+			try {
+				Scanner input = new Scanner(System.in);//Create Input Scanner
+				System.out.print("Please Enter Your Age:");
+			    myAge = input.nextInt();
+			
+			    System.out.println(" \nYour Age is: "+ myAge);
+			    isValid = true;
+			
+			}   
+			    catch (Exception exception) {
+				System.out.println("'Please Enter A Positive Number'");
+			}
+		  }
+		}
+     }
+	
 
-	}
 
-}
